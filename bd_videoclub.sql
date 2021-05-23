d5957bd837dc728abca379e342129ae13de36d55
Akiuro <39047175+Akiuro@users.noreply.github.com>
2021-05-24 01:23:54 +0200

    Add files via upload

--- bd_videoclub.sql
+++ bd_videoclub.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-05-2021 a las 11:56:47
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Tiempo de generación: 24-05-2021 a las 01:22:33
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -68,6 +68,7 @@ CREATE TABLE `peliculas` (
   `pais` varchar(30) NOT NULL,
   `soporte` varchar(15) NOT NULL,
   `cantidad_disponible` int(4) NOT NULL,
+  `precio` float NOT NULL,
   `id` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -75,9 +76,15 @@ CREATE TABLE `peliculas` (
 -- Volcado de datos para la tabla `peliculas`
 --
 
-INSERT INTO `peliculas` (`nom_pelicula`, `nom_original`, `genero_principal`, `genero_secundario`, `imagen`, `sinopsis`, `anio`, `pais`, `soporte`, `cantidad_disponible`, `id`) VALUES
-('Mortal Kombat', 'Mortal Kombat (2021)', 'Aventuras', 'Ciencia Ficción', 'assets\\images\\films\\mortal_kombat.png', 'En \"Mortal Kombat\", Cole Young, un luchador de MMA acostumbrado a recibir palizas a cambio de plata, no sabe lo que heredó, ni por qué el emperador Shang Tsung del Mundo Exterior mandó a Sub-Zero, su mejor guerrero, un criomante de otro mundo, a cazarlo. Cole teme por la seguridad de su familia, y sale en busca de Sonya Blade por indicación de Jax, un comandante de las Fuerzas Especiales que tiene la misma marca rara de nacimiento que Cole, con forma de dragón. Cole llega pronto al templo de Lord Raiden, un Dios Antiguo protector de la Tierra que ofrece refugio a quienes portan la marca. Allí, Cole entrena con los guerreros expertos Liu Kang, Kung Lao y Kano, el mercenario rebelde, y se prepara para unirse a los mayores campeones de la Tierra en el combate contra los enemigos del Mundo Exterior en una arriesgada batalla por el universo. Pero ¿sentirá la presión suficiente para desbloquear a tiempo su arcana (el inmenso poder que proviene de su alma) no solo para salvar a su familia, sino también para vencer al Mundo Exterior para siempre?\r\n', 2021, 'Estados Unidos', 'Fisico', 20, 1),
-('Test', 'OriginalTest', 'Aventuras', 'Ciencia Ficción', 'ImagenTest', 'La sinopsis de esta pelicula es un test', 2020, 'Españita', 'Video', 45, 2);
+INSERT INTO `peliculas` (`nom_pelicula`, `nom_original`, `genero_principal`, `genero_secundario`, `imagen`, `sinopsis`, `anio`, `pais`, `soporte`, `cantidad_disponible`, `precio`, `id`) VALUES
+('Mulán (2020)', 'Hua Mulan', 'Acción', 'Aventuras', 'assets/images/films/mulan.jpg', 'Ante la amenaza de la invasión de los hunos, el emperador chino recluta a un varón de cada familia para luchar en el ejército. Fa Mulán (Liu Yifei) decide disfrazarse de hombre para proteger a su padre, un héroe de guerra con una cojera permanente. Mulán deberá proteger su verdadera identidad y evitar que la descubran, pero su secreto acabará por salir a la luz.\n\n', 2020, 'Estados Unidos', 'Digital', 30, 19.95, 10),
+('La vida es bella', 'La vita è bella', 'Dramática', 'Dramática', 'assets/images/films/lavidaesbella.jpg', 'En 1939, a punto de estallar la Segunda Guerra Mundial (1939-1945), el extravagante Guido llega a Arezzo, en la Toscana, con la intención de abrir una librería. Allí conoce a la encantadora Dora y, a pesar de que es la prometida del fascista Rodolfo, se casa con ella y tiene un hijo. Al estallar la guerra, los tres son internados en un campo de exterminio, donde Guido hará lo imposible para hacer creer a su hijo que la terrible situación que están padeciendo es tan sólo un juego.', 1997, 'Italia', 'Físico', 20, 9.95, 11),
+('Los Increíbles 2', 'Incredibles 2', 'Animación', 'Acción', 'assets/images/films/increibles2.jpg', 'Helen es reclutada para ayudar a que la acción vuelva a la vida de los Súper, mientras Bob se enfrenta a la rutina de su vida diaria \"normal\" en el hogar. En casa debe lidiar con un bebé que está a punto de descubrir sus superpoderes. Mientras tanto un nuevo villano trama un plan brillante y peligroso que lleva a pique toda la estabilidad conseguida y que solo Los Increíbles podrán afrontar juntos.', 2018, 'Estados Unidos', 'Fisico', 30, 14.95, 12),
+('A todo gas', 'The Fast and the Furious', 'Acción', 'Aventuras', 'assets/images/films/fastandfurious.jpg', 'Una misteriosa banda de delincuentes se dedica a robar camiones en marcha desde vehículos deportivos. La policía decide infiltrar un hombre en el mundo de las carreras ilegales para descubrir posibles sospechosos. El joven y apuesto Brian entra en el mundo del tunning donde conoce a Dominic, rey indiscutible de este mundo y sospechoso número uno, pero todo se complicará cuando se enamore de su hermana.', 2001, 'Estados Unidos', 'Fisico', 30, 4.95, 13),
+('Mamma Mia!', 'Mamma Mia!', 'Comedia', 'Musical', 'assets/images/films/mammamia.jpg', 'Donna, una madre independiente y soltera, dueña de un pequeño hotel en una idílica isla griega, está a punto de dejar que se marche Sophie, la hija a la que ha criado sola. Donna ha invitado a sus dos mejores amigas a la boda de su hija, Rosie, una mujer práctica y lógica, y Tanya, rica y multidivorciada. Las dos son ex miembros de su antigua banda, Donna and the Dynamos. Por su parte, Sophie también ha hecho tres invitaciones muy especiales.\n\nDecidida a encontrar un padre para que la lleve al altar, invita a tres hombres que visitaron la paradisíaca isla hace 20 años. Durante las siguientes caóticas y mágicas 24 horas, florecerán nuevos amores y se reavivarán viejos romances en una isla llena de posibilidades.', 2008, 'Reino Unido', 'Digital', 30, 4.95, 14),
+('Pokemon: Diancie y la crisálida de la destrucción', 'Pokemon The Movie XY Hakai No Mayu to Diancie', 'Animación', 'Aventuras', 'assets/images/films/pkmndiance.jpg', 'En el subterráneo Dominio Diamante, donde muchos Carbink viven, el Pokémon singular Diancie es quien está al mando. El Diamante Corazón, que sostiene el reino, está empezando a desmoronarse, y Diancie no es todavía lo suficientemente fuerte para crear uno nuevo. Mientras busca la ayuda del Pokémon legendario Xerneas, Diancie se encuentra con un grupo de ladrones que quieren hacerse con su poder para crear diamantes y que despiertan al Pokémon legendario Yveltal, que dormía en su crisálida. ¿Podrán Ash y sus amigos ayudar a Diancie a descubrir su verdadero poder, detener la furia de Yveltal y salvar el Dominio Diamante?', 2014, 'Japón', 'Fisico', 30, 14.95, 15),
+('Avatar', 'Avatar', 'Ciencia Ficción', 'Aventuras', 'assets/images/films/avatar.jpg', 'Jake Sully es un ex-marine confinado en una silla de ruedas que, a pesar de su cuerpo tullido, todavía es un guerrero de corazón. Jake ha sido reclutado para viajar a Pandora, donde las corporaciones están extrayendo un mineral extraño que es la clave para resolver los problemas de la crisis energética de la Tierra. Al ser tóxica la atmósfera de Pandora, ellos han creado el programa Avatar, en el cual los humanos ¿conductores¿ tienen sus conciencias unidas a un avatar, un cuerpo biológico controlado de forma remota que puede sobrevivir en el aire letal. Estos cuerpos están creados genéticamente de ADN humano, mezclado con ADN de los nativos de Pandora¿ los Na\'vi. Ya en su forma avatar, Jake puede caminar otra vez. Ha recibido la misión de infiltrarse entre los Na\'vi, los cuales se han convertido en el mayor obstáculo para la extracción del mineral. Pero una bella Na\'vi, Naytiri, salva la vida de Jake, y todo cambia. Jake es admitido en su clan y aprende a ser uno de ellos, lo cual le hace someterse a muchas pruebas y aventuras. Según la relación de Jake con su profesora Neytiry se va intensificando, él aprende a respetar la vida de los Na\'vi y decide encontrar su lugar entre ellos. Pronto se enfrentará a la mayor de las pruebas cuando tenga que dirigir una batalla épica que decidirá nada menos que el destino de su nuevo mundo.', 2009, 'Estados Unidos', 'Físico', 30, 4.95, 16),
+('Mortal Kombat', 'Mortal Kombat', 'Ciencia Ficción', 'Acción', 'assets/images/films/mortal_kombat.png', 'trhrtyhnyt4ij6tyuj', 2020, 'Estados Unidos', 'Digital', 30, 14.95, 17);
 
 -- --------------------------------------------------------
 
@@ -116,7 +123,8 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`nom_usuario`, `email`, `estado`, `password`, `tipo_usuario`, `id`) VALUES
 ('Akiuro', 'adrianmendozasantano@gmail.com', 1, '$2y$10$.40gkP6z2076iucrPYnwJueVZnpkVoKzuSaL0L8l6jne5h7j2uTEy', 'administrador', 34),
-('Paco', 'Fiestas@gmail.com', 1, '$2y$10$rnTZiA3.H7Wkcb5UqdZIdeUMRQxhmqtM8awTiXLzT4owwkHaVEXui', 'normal', 35);
+('Paco', 'Fiestas@gmail.com', 1, '$2y$10$rnTZiA3.H7Wkcb5UqdZIdeUMRQxhmqtM8awTiXLzT4owwkHaVEXui', 'normal', 35),
+('Isabel', 'sabudoo@gmail.com', 1, '$2y$10$TEMUOVNSaAyFh1b3bRwvIeNhl6kVR08lMHN3BGjTEiB5DcuYM10.K', 'normal', 36);
 
 -- --------------------------------------------------------
 
@@ -183,13 +191,13 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- Restricciones para tablas volcadas
